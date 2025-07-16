9e7d24c83d7ee90aaf816a1e99cad91b5565cfb4
MRBOX <olamigokeadebayo90@gmail.com>
2025-07-17 00:58:05 +0100

    Starting project

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import { Text } from "react-native";
+
+export default function RootLayout() {
+  return (
+  <>
+
+   <Stack> 
+    <Stack.Screen name="index" options={{title: "Home"}} />
+    <Stack.Screen name="login" options={{title: "Login"}} />
+   </Stack>
+  </>
+
+  ); 
+}
---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function loginScreen() {
+  return (
+  <View>
+    {""}
+    <Text>Hello this is the Login Page</Text>
+  </View>
+  );
+}
